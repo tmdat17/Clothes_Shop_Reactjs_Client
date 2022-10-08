46a971409b2aaca78fd2fdf4e53def893b8237d3
Dat17 <tmdatsc.vl@gmail.com>
2022-10-08 20:40:02 +0700

    edited responsive page Signup

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -8,97 +8,105 @@ function Signup() {
         document.title = 'Đăng ký';
     }, []);
     return (
-        <Container fluid>
-            <div className={' my-3 my-sm-3 my-md-5 ' + clsx(styles.signupForm)}>
-                <h2 className="text-center">TẠO TÀI KHOẢN</h2>
-                <form className="d-flex justify-content-center" method="POST" action="#">
-                    <div className="">
-                        <input
-                            type="text"
-                            size={45}
-                            required=""
-                            name="name"
-                            placeholder="Họ và tên"
-                            className={
-                                ' d-block input-group-md my-3 my-sm-3 my-md-4 mx-auto ' + clsx(styles.inputInfor)
-                            }
-                        />
-                        <input
-                            type="tel"
-                            size={45}
-                            required=""
-                            name="number_phone"
-                            placeholder="Số điện thoại"
-                            className={
-                                ' d-block input-group-md my-3 my-sm-3 my-md-4 mx-auto ' + clsx(styles.inputInfor)
-                            }
-                        />
-                        <input
-                            type="date"
-                            size={45}
-                            required=""
-                            name="birthday"
-                            placeholder="dd/mm/yyyy"
-                            className={
-                                ' d-block input-group-md my-3 my-sm-3 my-md-4 mx-auto ' + clsx(styles.inputInfor)
-                            }
-                            style={{ width: '55%', fontWeight: '400' }}
-                        />
-                        <input
-                            type="password"
-                            size={45}
-                            required=""
-                            name="password"
-                            placeholder="Mật khẩu"
-                            className={
-                                ' d-block input-group-md my-3 my-sm-3 my-md-4 mx-auto ' + clsx(styles.inputInfor)
-                            }
-                        />
-                        <input
-                            type="password"
-                            size={45}
-                            required=""
-                            name="acpt_password"
-                            placeholder="Xác nhận lại mật khẩu"
-                            className={
-                                ' d-block input-group-md my-3 my-sm-3 my-md-4 mx-auto ' + clsx(styles.inputInfor)
-                            }
-                        />
-                        <Row className=" mx-auto ">
-                            <Col xs={12} sm={12} md={12} className=" text-center">
-                                <input
-                                    className="form-check-input my-4 me-2"
-                                    type="checkbox"
-                                    defaultValue=""
-                                    id="acpt_cookie"
-                                />
-                                <label className="text-secondary my-3 fs-5 text-center" htmlFor="acpt_cookie">
-                                    Tôi đồng ý với các điều khoản và điều kiện, chính sách bảo mật và chính sách cookie
-                                </label>
-                            </Col>
-                            <Col xs={12} sm={12} md={12} className=" text-center">
-                                <button
-                                    className={' w-25 fs-3 ms-md-4 mx-auto mb-3 ' + clsx(styles.signupBtn)}
-                                    type="submit"
-                                >
-                                    Tạo
-                                </button>
-                            </Col>
-                        </Row>
-                        <p className="mx-auto" style={{ fontWeight: '300', width: '600px' }}>
-                            Nếu sdt hoặc email của bạn báo đã tồn tại, bấm{' '}
-                            {
-                                <a className="fw-bold" href="#">
-                                    vào đây
-                                </a>
-                            }{' '}
-                            và login với sdt hoặc email đó với một mật khẩu bất kì để kích hoạt tài khoản hoặc liên hệ
-                            với bộ phận cskh của Levents để được hỗ trợ
-                        </p>
-                    </div>
-                </form>
-            </div>
-        </Container>
+        <>
+            <Container fluid>
+                <div className={' my-3 my-sm-3 my-md-5 ' + clsx(styles.signupForm)}>
+                    <h2 className="text-center" style={{ fontWeight: '400', marginBottom: '2rem' }}>
+                        TẠO TÀI KHOẢN
+                    </h2>
+                    <form
+                        className=" d-flex justify-content-start d-sm-flex justify-content-sm-start d-md-flex justify-content-md-center"
+                        method="POST"
+                        action="#"
+                    >
+                        <div className="">
+                            <input
+                                type="text"
+                                required=""
+                                name="name"
+                                placeholder="Họ và tên"
+                                className={
+                                    ' d-block my-3 my-sm-3 my-md-4 w-sm-25 form-control ' + clsx(styles.inputInfor)
+                                }
+                            />
+
+                            <input
+                                type="tel"
+                                required=""
+                                name="number_phone"
+                                placeholder="Số điện thoại"
+                                className={
+                                    ' d-block my-3 my-sm-3 my-md-4 w-sm-25 form-control ' + clsx(styles.inputInfor)
+                                }
+                            />
+                            <input
+                                type="date"
+                                required=""
+                                name="birthday"
+                                placeholder="dd/mm/yyyy"
+                                className={
+                                    ' d-block my-3 my-sm-3 my-md-4 w-sm-25 form-control ' + clsx(styles.inputInfor)
+                                }
+                            />
+                            <input
+                                type="password"
+                                required=""
+                                name="password"
+                                placeholder="Mật khẩu"
+                                className={
+                                    ' d-block my-3 my-sm-3 my-md-4 w-sm-25 form-control ' + clsx(styles.inputInfor)
+                                }
+                            />
+                            <input
+                                type="password"
+                                required=""
+                                name="acpt_password"
+                                placeholder="Xác nhận lại mật khẩu"
+                                className={
+                                    ' d-block my-3 my-sm-3 my-md-4 w-sm-25 form-control ' + clsx(styles.inputInfor)
+                                }
+                            />
+                            <Row className=" mx-auto ">
+                                <Col xs={12} sm={12} md={12} className=" my-2 ">
+                                    <input
+                                        className="form-check-input mt-4 me-2"
+                                        type="checkbox"
+                                        defaultValue=""
+                                        id="acpt_cookie"
+                                    />
+                                    <label
+                                        className="text-secondary mt-4 "
+                                        style={{ fontSize: 16 }}
+                                        htmlFor="acpt_cookie"
+                                    >
+                                        Tôi đồng ý với các điều khoản và điều kiện, chính sách bảo mật và <br /> chính
+                                        sách cookie
+                                    </label>
+                                </Col>
+                                <Col xs={12} sm={12} md={12} className=" text-center">
+                                    <button
+                                        className={' w-50 fs-3 ms-md-4 mx-auto ' + clsx(styles.signupBtn)}
+                                        type="submit"
+                                    >
+                                        Tạo
+                                    </button>
+                                </Col>
+                            </Row>
+                        </div>
+                    </form>
+                </div>
+                <p className="mx-auto " style={{ width: '70%', fontSize: '1rem' }}>
+                    Nếu sdt hoặc email của bạn báo đã tồn tại, bấm{' '}
+                    {
+                        <a className="fw-bold" href="#">
+                            vào đây
+                        </a>
+                    }{' '}
+                    và login với sdt hoặc email đó với một mật khẩu bất kì để kích hoạt tài khoản hoặc liên hệ với bộ
+                    phận cskh của Levents để được hỗ trợ
+                </p>
+            </Container>
+        </>
     );
 }
 
